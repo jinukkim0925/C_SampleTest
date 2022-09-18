c2c437e679244cbfa806bd0990a2b475f12db532
김진욱 <55441664+jinukkim0925@users.noreply.github.com>
2022-09-18 15:07:19 +0900

    Create num.c

---
+++ test/num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct BigNum {
+    int i_digit[100];
+    int d_digit[100];
+    int i_total_digit;
+    int d_total_digit;
+    char sign;
+};
+
+int mina(){
+    struct BigNum bn;
+    scanf("");
+    
+}
