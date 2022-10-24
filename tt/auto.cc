57990c95c215b892e448a2d72a39d3f22d3f9c47
김진욱 <55441664+jinukkim0925@users.noreply.github.com>
2022-10-24 09:19:33 +0900

    Create auto.cc

---
+++ tt/auto.cc
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <iostream>
+
+
+int aaaaa(){
+    int a;
+    
+}
+
+int main() {
+
+    auto Doo =[&](double a, bool b) -> int
+    {
+        int Result; 
+        int aaa = a;
+        
+        Result = 123 + 3;
+        return Result;
+    };
+    
+    printf("%d %d\n", Doo(10, true), aaaaa());
+
+}
